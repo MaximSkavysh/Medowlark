9648ec99647f5b96a4d681d05485a8b0530254f2
Maksim Skavysh <maksimskavysh@Maksims-MacBook-Pro.local>
2018-11-26 20:50:44 +0300

    Add fortune to module

--- meadowlark.js
+++ meadowlark.js
@@ -2,31 +2,33 @@
  * Created by SkavyshM on 11/26/2018.
  */
 const express = require('express');
-const handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+const handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
+const fortune = require('./lib/fortune')
 const PORT = process.env.port || 3000;
 let app = express();
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', PORT);
 app.use(express.static(__dirname + '/public'));
-app.get('/',(req, res) =>{
+
+app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/about',(req, res) =>{
-    res.render('about');
+app.get('/about', (req, res) => {
+    res.render('about', {fortune : fortune.getFortune()});
 });
-app.use((req,res) =>{
+app.use((req, res) => {
     res.status(404);
     res.render('404');
 });
 
-app.use((err, req, res, next) =>{
-   console.error(err.stack);
+app.use((err, req, res, next) => {
+    console.error(err.stack);
     res.status(500);
     res.render('500');
 });
 
-app.listen(app.get('port'), () =>{
-   console.log('Server is starting on port: ' + app.get('port'));
+app.listen(app.get('port'), () => {
+    console.log('Server is starting on port: ' + app.get('port'));
 });
